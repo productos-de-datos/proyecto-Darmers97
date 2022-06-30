3e3579ebad7fbf3fb03cdb7dd0bf74e18bb7e632
Sergio Arango Jaramillo <47546473+Darmers97@users.noreply.github.com>
2022-06-29 21:26:24 -0500

    Update ingest_data.py
    ingest data commit

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -13,10 +13,24 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
-    raise NotImplementedError("Implementar esta función")
+    import requests
+    
+    
+    path = "https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/"
 
-
+    for year in range(1995, 2022):
+        if year in [2016, 2017]:
+            url = path + "{}.xls?raw=true".format(year)
+            rsp = requests.get(url, allow_redirects=True)
+            open('data_lake/landing/{}.xls'.format(year),"wb").write(rsp.content)
+        else:
+            url = path + "{}.xlsx?raw=true".format(year)
+            rsp = requests.get(url, allow_redirects=True)
+            open('data_lake/landing/{}.xlsx'.format(year),"wb").write(rsp.content)
+            
+            
+            
 if __name__ == "__main__":
     import doctest
-
+    ingest_data()
     doctest.testmod()
