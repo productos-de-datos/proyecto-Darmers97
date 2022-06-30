7c5c1916b397bb5988e2fccea0e784f641f939e1
Sergio Arango Jaramillo <47546473+Darmers97@users.noreply.github.com>
2022-06-29 21:16:11 -0500

    Update compute_daily_prices.py
    compute daily prices commit

--- src/data/compute_daily_prices.py
+++ src/data/compute_daily_prices.py
@@ -12,10 +12,35 @@ def compute_daily_prices():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+    import pandas as pd
+    
+    
+    
+    data_cleanner = pd.read_csv('data_lake/cleansed/precios-horarios.csv',
+                     index_col=None, header=0)
+    data_cleanner['Fecha'] = pd.to_datetime(data_cleanner['Fecha'], format='%Y-%m-%d')
+    df = data_cleanner.groupby(data_cleanner['Fecha'])[
+        'Precio'].mean().reset_index()
+
+    df.to_csv(
+        'data_lake/business/precios-diarios.csv', index=None)
+    
+    
+    
+#raise NotImplementedError("Implementar esta función")
 
 
+def test_save_no_format():
+
+    import pandas as pd
+    read_f = pd.read_csv(
+                'data_lake/business/precios-diarios.csv')
+    assert str(read_f['Fecha'].dtypes) == "object"
+    
+    
+    
+    
 if __name__ == "__main__":
     import doctest
-
+    compute_daily_prices()
     doctest.testmod()
