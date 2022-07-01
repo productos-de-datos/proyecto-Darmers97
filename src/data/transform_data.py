e903f5b1d60bf4df0ebf469859a0f7977f09777a
Sergio Arango Jaramillo <47546473+Darmers97@users.noreply.github.com>
2022-06-30 20:29:17 -0500

    Update transform_data.py
    transform data commit 2

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -1,36 +1,34 @@
 def transform_data():
     """Transforme los archivos xls a csv.
-
     Transforme los archivos data_lake/landing/*.xls a data_lake/raw/*.csv. Hay
     un archivo CSV por cada archivo XLS en la capa landing. Cada archivo CSV
     tiene como columnas la fecha en formato YYYY-MM-DD y las horas H00, ...,
     H23.
-
     """
-import pandas as pd
+    import pandas as pd
 
-for f in range(1995, 2022):
-    if f in range(1995, 2000):
-        read_f = pd.read_excel(
-            'data_lake/landing/{}.xlsx'.format(f), header=3)
-        read_f.to_csv(
-            'data_lake/raw/{}.csv'.format(f), index=None)
-    elif f in range(2000, 2018):
-        if f in [2016, 2017]:
+    for f in range(1995, 2022):
+        if f in range(1995, 2000):
             read_f = pd.read_excel(
-                'data_lake/landing/{}.xls'.format(f), header=2)
+                'data_lake/landing/{}.xlsx'.format(f), header=3)
             read_f.to_csv(
                 'data_lake/raw/{}.csv'.format(f), index=None)
+        elif f in range(2000, 2018):
+            if f in [2016, 2017]:
+                read_f = pd.read_excel(
+                    'data_lake/landing/{}.xls'.format(f), header=2)
+                read_f.to_csv(
+                    'data_lake/raw/{}.csv'.format(f), index=None)
+            else:
+                read_f = pd.read_excel(
+                    'data_lake/landing/{}.xlsx'.format(f), header=2)
+                read_f.to_csv(
+                    'data_lake/raw/{}.csv'.format(f), index=None)
         else:
             read_f = pd.read_excel(
-                'data_lake/landing/{}.xlsx'.format(f), header=2)
+                'data_lake/landing/{}.xlsx'.format(f), header=0)
             read_f.to_csv(
                 'data_lake/raw/{}.csv'.format(f), index=None)
-    else:
-        read_f = pd.read_excel(
-            'data_lake/landing/{}.xlsx'.format(f), header=0)
-        read_f.to_csv(
-            'data_lake/raw/{}.csv'.format(f), index=None)
 
 
 
